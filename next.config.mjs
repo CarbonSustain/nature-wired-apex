5c92aaac91048a0c01b96bf321ad63d44788d604
kushaalrajiv <kushaalrajiv@gmail.com>
2026-03-22 20:11:52 +0530

    fix nextauthurl on amplify

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,6 @@ const nextConfig = {
     AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
     AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
     AUTH_SECRET: process.env.AUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
